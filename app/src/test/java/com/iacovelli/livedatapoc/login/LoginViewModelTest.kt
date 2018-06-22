c416f6798aa685b1c6f5f9d3cf765baf4128208a
douglasiacovelli <douglas.iacovelli@gmail.com>
2018-06-22 12:01:14 -0300

    Creates Unit Test for LoginViewModel and adds Mockito-kotlin

---
+++ app/src/test/java/com/iacovelli/livedatapoc/login/LoginViewModelTest.kt
@@ -0,0 +1,75 @@
+package com.iacovelli.livedatapoc.login
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.Observer
+import com.iacovelli.livedatapoc.R
+import com.iacovelli.livedatapoc.common.SingleEvent
+import com.iacovelli.livedatapoc.model.LoginCredential
+import com.nhaarman.mockito_kotlin.*
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class LoginViewModelTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    private val messageObserver: Observer<SingleEvent<Int>> = mock()
+    private val userLoggedObserver: Observer<SingleEvent<Boolean>> = mock()
+    private val formValidator: LoginFormValidator = mock()
+    private val repository: LoginRepository = mock()
+    private val messageCaptor = argumentCaptor<SingleEvent<Int>>()
+
+    private lateinit var viewModel: LoginViewModel
+
+    @Before
+    fun setup() {
+        viewModel = LoginViewModel(repository, formValidator)
+        viewModel.message.observeForever(messageObserver)
+        viewModel.userLogged.observeForever(userLoggedObserver)
+    }
+
+    @Test
+    fun whenFormIsInvalidItShouldNotCallRepository() {
+        given(formValidator.isValid(anyString(), anyString()))
+                .willReturn(false)
+        viewModel.onUserSubmit()
+
+        verify(messageObserver).onChanged(messageCaptor.capture())
+        verify(userLoggedObserver, never()).onChanged(anyOrNull())
+        assertEquals(R.string.invalid_form, messageCaptor.firstValue.getContent())
+        verify(repository, never()).login(anyOrNull())
+    }
+
+    @Test
+    fun whenFormIsValidItShouldCallRepository() {
+        val loginCredentialCaptor = argumentCaptor<LoginCredential>()
+        val userLoggedCaptor = argumentCaptor<SingleEvent<Boolean>>()
+
+        val email = "user@mail.com"
+        val password = "password"
+        viewModel.email.value = email
+        viewModel.password.value = password
+
+        given(formValidator.isValid(anyString(), anyString()))
+                .willReturn(true)
+        given(repository.login(any())).willReturn(true)
+
+        viewModel.onUserSubmit()
+        verify(repository).login(loginCredentialCaptor.capture())
+
+        assertEquals(email, loginCredentialCaptor.firstValue.email)
+        assertEquals(password, loginCredentialCaptor.firstValue.password)
+
+        verify(userLoggedObserver).onChanged(userLoggedCaptor.capture())
+        assertTrue(userLoggedCaptor.firstValue.getContent()!!)
+
+        verify(messageObserver, never()).onChanged(anyOrNull())
+    }
+}
