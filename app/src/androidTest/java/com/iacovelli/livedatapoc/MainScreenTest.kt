3afad2b718b5434d99681ae279d769b95a2789ec
douglasiacovelli <douglas.iacovelli@gmail.com>
2018-07-09 10:39:18 -0300

    creates instrumented test for incorrect form

--- app/src/androidTest/java/com/iacovelli/livedatapoc/MainScreenTest.kt
+++ app/src/androidTest/java/com/iacovelli/livedatapoc/MainScreenTest.kt
@@ -16,24 +16,28 @@ import org.junit.Before
 import org.junit.runner.RunWith
 import org.junit.Rule
 import org.junit.Test
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.RootMatchers.withDecorView
+import android.support.test.espresso.Espresso.onView
+import org.hamcrest.CoreMatchers.*
 
 
 @RunWith(AndroidJUnit4::class)
 class MainScreenTest {
     private val VALID_EMAIL = "address@mail.com"
     private val VALID_PASSWORD = "12345678"
+    private val INVALID_EMAIL = "address.com"
+    private val INVALID_PASSWORD = "1234"
 
     @Rule
     @JvmField
     val instantExecutorRule = InstantTaskExecutorRule()
 
     @get:Rule
-    val activityActivityTestRule = ActivityTestRule<MainActivity>(MainActivity::class.java, false, false)
+    val activityTestRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
 
     @Test
     fun checkFormFilledCorrectlyPasses() {
-        activityActivityTestRule.launchActivity(null)
-
         onView(withId(R.id.email_field))
                 .check(matches(isDisplayed()))
                 .perform(typeText(VALID_EMAIL))
@@ -49,4 +53,28 @@ class MainScreenTest {
         onView(withId(R.id.list))
                 .check(matches(isDisplayed()))
     }
+
+    @Test
+    fun whenFormIsFilledIncorrectlyItDoesNotPass() {
+        onView(withId(R.id.email_field))
+                .check(matches(isDisplayed()))
+                .check(matches(withText("")))
+                .perform(typeText(INVALID_EMAIL))
+
+        onView(withId(R.id.password_field))
+                .check(matches(isDisplayed()))
+                .check(matches(withText("")))
+                .perform(typeText(INVALID_PASSWORD))
+
+        onView(withId(R.id.submit_button))
+                .check(matches(isDisplayed()))
+                .perform(click())
+
+        onView(allOf(withId(android.support.design.R.id.snackbar_text), withText(R.string.invalid_form)))
+                .check(matches(isDisplayed()))
+
+    }
+
+    private fun getString(resId: Int) = activityTestRule.activity.getString(resId)
+
 }
