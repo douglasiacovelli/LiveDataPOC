740bc7cc2e6211cc52466597bfb5903a240d9820
Douglas Iacovelli <douglas.iacovelli@youse.com.br>
2018-07-17 17:43:58 -0300

    removes unused import

--- app/src/main/java/com/iacovelli/livedatapoc/list/ListFragment.kt
+++ app/src/main/java/com/iacovelli/livedatapoc/list/ListFragment.kt
@@ -1,6 +1,5 @@
 package com.iacovelli.livedatapoc.list
 
-import android.content.DialogInterface
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
